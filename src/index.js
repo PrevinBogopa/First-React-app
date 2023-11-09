bcb7461108b69b237be125244deb645f96db89db
PrevinBogopa <previnbogopa@icloud.com>
2023-11-09 11:59:52 +0200

    update mobile

--- src/index.js
+++ src/index.js
@@ -118,15 +118,9 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
+  const openHour = 7;
+  const closeHour = 20;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  // else alert("Sorry we're closed");
-
-  // if (!isOpen) return <p>CLOSED</p>;
 
   return (
     <footer className="footer">
